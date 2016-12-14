621335a36a195b21656b316fbc8c8606039c6376
Victor <devmosedu@yandex.ru>
2016-12-14 15:08:12 +0300

    Добавлены картинки для команды

--- themes/v01/site/block-team.php
+++ themes/v01/site/block-team.php
@@ -21,9 +21,45 @@ use yii\web\JsExpression;
         <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12">
             <div class="row">
                 <?php
-                    $aSpeaker = [];
+                    $aSpeaker = [
+                        [
+                            'src' => '/img/team/woman-437x437.jpg',
+                            'name' => 'Анастасия Горчакова',
+                            'prof' => 'Руководитель проекта',
+                        ],
+                        [
+                            'src' => '/img/team/man-300x300.jpg',
+                            'name' => 'Сергей Дробышев',
+                            'prof' => 'Координатор проекта',
+                        ],
+                        [
+                            'src' => '/img/team/woman-251x251.jpg',
+                            'name' => 'Ольга Кондратенко',
+                            'prof' => 'Пресс-служба',
+                        ],
+                        [
+                            'src' => '/img/team/woman-310x310.jpg',
+                            'name' => 'Анастасия Горчакова',
+                            'prof' => 'Руководитель проекта',
+                        ],
+                        [
+                            'src' => '/img/team/man-267x267.jpg',
+                            'name' => 'Анастасия Горчакова',
+                            'prof' => 'Руководитель проекта',
+                        ],
+
+
+
+
+                    ];
                     for($i=0; $i<3; $i++) {
-                        echo $this->render('one_team');
+                        echo $this->render(
+                            'one_team',
+                            [
+                                'aPerson' => $aSpeaker[$i],
+                            ]
+                        );
+//                        break;
                     }
                 ?>
             </div>
--- themes/v01/site/one_team.php
+++ themes/v01/site/one_team.php
@@ -6,11 +6,18 @@ use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
 /** @var $model app\modules\lectors\models\Lesson */
+/** @var $aPerson array */
+
+//echo nl2br(print_r($aPerson, true));
+//return;
 
 ?>
 
 <div class="col-sm-4" style="text-align: center; padding-right: 24px; padding-left: 24px;">
-    <p style="width: 180px; height: 180px; border: 4px solid #ffffff; border-radius: 90px; background-color: #eeeeee; display: inline-block;"></p>
-    <h1 style="font-size: 26px;"><?= Html::encode('Фамилия Имя') ?></h1>
-    <p><?= Html::encode('Профессия') ?></p>
+    <p style="width: 180px; height: 180px; border: 4px solid #ffffff; border-radius: 90px; background-color: #eeeeee; display: inline-block; overflow: hidden;">
+        <img src="<?= $aPerson['src'] ?>" style="width: 100%;" />
+    </p>
+
+    <h1 style="font-size: 15px; font-weight: bold;"><?= Html::encode($aPerson['name']) ?></h1>
+    <p style="font-size: 15px;"><?= Html::encode($aPerson['prof']) ?></p>
 </div>
